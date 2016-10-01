dadda33ab0a89074f58976599752df1c0a2510c3
Ivan Chernyshev <ivanchernyshev@pioneers.berkeley.edu>
2016-10-01 21:59:27 +0000

    Completed the first draft of the virtual device message protocol

--- hibike/virtual_device.py
+++ hibike/virtual_device.py
@@ -30,23 +30,37 @@ year = 1
 id = random.randint(0, 0xFFFFFFFFFFFFFFFF)
 delay = 0
 subResponseTime = 0
+update_count = 1
 
 while True:
+        if ((subResponseTime != 0) && ((time.time() - subResponseTime) >= (delay * .001 * update_count)): #If the time equal to the delay has elapsed since the previous data update, send a data update                                            
+            if device_type in (hm.deviceTypes["LimitSwitch"]):  # If the device type is a limit switch, send 4 arbitrary booleans
+                 statusVars = struct.pack("<????", (true, false, false, true))
+            if device_type in (hm.deviceTypes["ServoControl"]): # If the device is a servo control, send an empty packet
+                 statusVars = struct.pack("<", ())
+
+            dataUpdate = HibikeMessage(hm.messageTypes("DataUpdate", statusVars)
+            hm.send(conn, dataUpdate)
+             
 	msg = hm.read(conn)
 	if not msg:
-                if ((delay != 0) && (subResponseTime != 0) && ((time.time() - subResponseTime) % delay == 0)#If it's time to send a data update, do what's below
-                    if (device_type == 00x0) # If the device type is a limit switch, send 4 booleans via a data update
-                        statusVars = struct.pack("<????", (true, false, false, true))
-                        dataUpdate = HibikeMessage(0x02, statusVars)
-                        hm.send(conn, dataUpdate)
-                    if 
-		time.sleep(.001)
-		continue
-	if msg.getmessageID() in (hm.messageTypes["SubscriptionRequest"],
-                delay = struct.unpack("<H")
-                hm.send(conn, hm.make_sub_response(device_type, year, id, delay))
-                subResponseTime = time.time()
-        if msg.getmessageID()in (hm.messageTypes["Ping"]):
-		hm.send(conn, hm.make_sub_response(device_type, year, id, delay))
-        if 
-        
+             time.sleep(.001)
+             continue
+	if msg.getmessageID() in (hm.messageTypes["SubscriptionRequest"]): #Update the delay and subscription time, and send a subscription response 
+             delay = struct.unpack("<H")
+             hm.send(conn, hm.make_sub_response(device_type, year, id, delay))
+             subResponseTime = time.time()
+        if msg.getmessageID() in (hm.messageTypes["Ping"]): # Send a subscription response 
+             hm.send(conn, hm.make_sub_response(device_type, year, id, delay))
+        if msg.getmessageID() in (hm.messageTypes["DeviceUpdate"]): # Send a parameter and a value in a device response
+             param, value = struct.unpack("<BI")
+             responsePayload = struct.pack("<BI", (param, value))
+             deviceResponse = HibikeMessage(hm.messageTypes["DeviceResponse"], responsePayload)
+             hm.send(conn, deviceResponse)
+        if msg.getmessageID() in (hm.messageTypes["DeviceStatus"]):  # Send a parameter and a value in a device response  
+             param = struct.unpack("<B")
+             value = 0000
+             responsePayload = struct.pack("<BI", (param, value))
+             deviceResponse = HibikeMessage(hm.messageTypes["DeviceResponse"], responsePayload)
+             hm.send(conn, deviceResponse)
+                                
