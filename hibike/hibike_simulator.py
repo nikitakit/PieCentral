7e0d8140a94e09c7b02f0d9576f0e3a985aa58d1
karthik-shanmugam <karthik@karthiks.me>
2016-10-27 17:00:22 -0700

    work on simulator

--- hibike/hibike_simulator.py
+++ hibike/hibike_simulator.py
@@ -1,4 +1,98 @@
 import time, random
+import threading
+import hibike_message as hm
+import queue
+
+fake_uids = [0 << 64, 7 << 64]
+
+
+uid_to_index = {}
+
+
+def hibike_process(badThingsQueue, stateQueue, pipeFromChild):
+    while pipeFromChild.recv()[0] != "ready":
+        pass
+    rrt = RuntimeReadThread(pipeFromChild)
+    ports = ['0', '1']
+    serials = [int(port) for port in ports]
+    instruction_queues = [queue.Queue() for _ in ports]
+    fake_device_queues = [queue.Queue() for _ in ports]
+    write_threads = [DeviceWriteThread(ser, iq, fake_device_queues) for ser, iq, fake_device_queue in zip(serials, instruction_queues, fake_device_queues)]
+    read_threads = [DeviceReadThread(ser, errorQueue, stateQueue, fake_device_queue) for ser, fake_device_queue in zip(serials, fake_device_queues)]
+    for read_thread in read_threads:
+        read_thread.start()
+    for write_thread in write_threads:
+        write_thread.start()
+    while True:
+        instruction, args = pipeFromChild.recv()
+        if instruction == "enumerate_all":
+            for instruction_queue in instruction_queues:
+                instruction_queue.put("ping", [])
+        elif instruction == "subscribe_device":
+            uid = args[0]
+            if uid in uid_to_index:
+                instruction_queues[uid_to_index[uid]].put("subscribe", args[1:])
+
+
+        # if instruction[0] == "enumerate_all":
+        #     stateQueue.put(["device_subscribed", 0, 0, []])
+
+# class RuntimeReadThread(threading.Thread):
+
+#     def __init__(self, ser, pipeFromChild):
+#         self.runtime_pipe = pipeFromChild
+
+#     def run(self):
+#         while True:
+#             instruction = self.runtime_pipe.recv()
+#             print(instruction)
+
+class DeviceWriteThread(threading.Thread):
+
+    def __init__(self, ser, instructionQueue, fake_device_queue):
+        self.ser = ser
+        self.queue = instructionQueue
+        self.fake_device_queue = fake_device_queue
+
+    def run(self):
+        while True:
+            instruction = self.queue.get()
+            fake_device_queue.put(instruction)
+
+class DeviceReadThread(threading.Thread):
+
+    def __init__(self, ser, errorQueue, stateQueue, fake_device_queue):
+        self.ser = ser
+        self.errorQueue = errorQueue
+        self.stateQueue = stateQueue
+        self.fake_device_queue = fake_device_queue
+        self.delay = 0
+        self.params = 0
+
+    def run(self):
+        while True:
+            instruction, args = fake_device_queue.get()
+            if instruction == "ping":
+                res = ["device_subscribed", [uid, delay, params]]
+            # message = hm.blocking_read(self.ser)
+            # print(message.getPayload())
+
+            # if message.getmessageID() == hm.messageTypes["SubscriptionResponse"]:
+            #     params, delay, device_type, year, id = struct.unpack("<HHHBQ", message.getPayload())
+            #     uid = (device_type << 72) | (year << 64) | id
+            #     port_to_uid[self.ser.port] = uid
+            #     print("got a subscription")
+
+            # elif message.getmessageID() == hm.messageTypes["DeviceData"]:
+            #     print("got some data")
+
+            # try to read a packet from serial port
+            # if an exception is caught (device disconnect) send it to errorQueue
+            # if a packet is read and the serial port has enumerated a uid, send the packet to stateQueue
+            pass
+
+
+
 
 #weight of each time interval for flipping.
 #for example, there is a 5% chance the data will flip within 1 sec
