293d37d27640ef46c9439f745ef7de0af548fc07
Karthik Shanmugam <karthiks@berkeley.edu>
2016-10-29 16:07:50 -0700

    clean up hibike_simulator.py

--- hibike/hibike_simulator.py
+++ hibike/hibike_simulator.py
@@ -33,20 +33,6 @@ def hibike_process(badThingsQueue, stateQueue, pipeFromChild):
             if uid in uid_to_index:
                 instruction_queues[uid_to_index[uid]].put(("subscribe", args))
 
-
-        # if instruction[0] == "enumerate_all":
-        #     stateQueue.put(["device_subscribed", 0, 0, []])
-
-# class RuntimeReadThread(threading.Thread):
-
-#     def __init__(self, ser, pipeFromChild):
-#         self.runtime_pipe = pipeFromChild
-
-#     def run(self):
-#         while True:
-#             instruction = self.runtime_pipe.recv()
-#             print(instruction)
-
 class DeviceWriteThread(threading.Thread):
 
     def __init__(self, ser, instructionQueue, fake_device_queue):
@@ -96,22 +82,6 @@ class DeviceReadThread(threading.Thread):
 
             self.stateQueue.put(res)
 
-            # message = hm.blocking_read(self.ser)
-            # print(message.getPayload())
-
-            # if message.getmessageID() == hm.messageTypes["SubscriptionResponse"]:
-            #     params, delay, device_type, year, id = struct.unpack("<HHHBQ", message.getPayload())
-            #     uid = (device_type << 72) | (year << 64) | id
-            #     port_to_uid[self.ser.port] = uid
-            #     print("got a subscription")
-
-            # elif message.getmessageID() == hm.messageTypes["DeviceData"]:
-            #     print("got some data")
-
-            # try to read a packet from serial port
-            # if an exception is caught (device disconnect) send it to errorQueue
-            # if a packet is read and the serial port has enumerated a uid, send the packet to stateQueue
-
 class FakeSubscriptionThread(threading.Thread):
 
     def __init__(self, uid, delay, params, fake_device_queue):
@@ -155,10 +125,7 @@ if __name__ == "__main__":
     pipeToChild.send(["enumerate_all", []])
     print(stateQueue.get())
     print(stateQueue.get())
-    pipeToChild.send(["subscribe_device", [0, 1000, ["switch0"]]])
+    pipeToChild.send(["subscribe_device", [0, 1000, ["switch0", "switch2"]]])
     print(stateQueue.get())
     while True:
         print(stateQueue.get())
-
-    # hi = Hibike()
-    # hi.subToDevices([('0x000100FFFFFFFFFFFFFFFF', 1)])
