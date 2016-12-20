dfe46c0c673f4de9ee95fb3264e1ceb2f7cc01a2
Yizhe Liu <yizow@berkeley.edu>
2016-12-20 04:42:34 -0800

    [HIBIKE] Example Device Refactor
    Rewrote example_device.* files to be more descriptive.
      * Updated example_device.cpp to actually use the new standard
      * Added a couple utility functions for read/ write to buffers
      * Added new values with different variable sizes
      * Added 'TODO' string for issues that yet need to be resolved
      * Modified example_device.json file to support new params
    
    Fixed a couple missing semicolons in limit switch to allow it to
    compile. Thanks @Karthik for catching those.

--- hibike/devices/ExampleDevice/example_device.cpp
+++ hibike/devices/ExampleDevice/example_device.cpp
@@ -1,69 +1,138 @@
 #include "example_device.h"
 
+// TODO: How are we using the params field in the json file?
+
 // each device is responsible for keeping track of it's own params
-uint32_t params[NUM_PARAMS];
-
-
 uint8_t value0;
 uint8_t value1;
 uint16_t value2;
 uint32_t value3;
 uint64_t value4;
+uint64_t value5;
+
 
 // normal arduino setup function, you must call hibike_setup() here
 void setup() {
   hibike_setup();
 }
 
+
 // normal arduino loop function, you must call hibike_loop() here
 // hibike_loop will look for any packets in the serial buffer and handle them
-void loop() {
 
+void loop() {
   // do whatever you want here
   // note that hibike will only process one packet per call to hibike_loop()
   // so exessive delays here will affect hibike.
-  value1++;
-  value2++;
-  value3++;
-  value4++;
+  value1 += 1;
+  value2 += 2;
+  value3 += 3;
+  value4 += 4;
+  value5 += 5;
 
   hibike_loop();
 }
 
 
-// you must implement this function. It is called when the device receives a DeviceUpdate packet.
-// the return value is the value field of the DeviceRespond packet hibike will respond with
-uint32_t device_update(uint8_t param, uint32_t value) {
-  if (param < NUM_PARAMS) {
-    params[param] = value;
-    return params[param];
-  }
-  return ~((uint32_t) 0);
-}
-
-// you must implement this function. It is called when the devie receives a DeviceStatus packet.
-// the return value is the value field of the DeviceRespond packet hibike will respond with
-uint32_t device_status(uint8_t param) {
-  if (param < NUM_PARAMS) {
-    return params[param];
-  }
-  return ~((uint32_t) 0);
-}
-
-
-// you must implement this function. It is called with a buffer and a maximum buffer size.
-// The buffer should be filled with appropriate data for a DataUpdate packer, and the number of bytes
-// added to the buffer should be returned. 
+// You must implement this function.
+// It is called when the device receives a Device Write packet.
+// Updates param to new value passed in data.
+//    param   -   Parameter index
+//    data    -   value to write, in bytes TODO: What endian?
+//    len     -   number of bytes in data
 //
-// You can use the helper function append_buf.
-// append_buf copies the specified amount data into the dst buffer and increments the offset
-uint8_t data_update(uint8_t* data_update_buf, size_t buf_len) {
-  uint8_t offset = 0;
-  append_buf(data_update_buf, &offset, (uint8_t *)&value0, sizeof(value0));
-  append_buf(data_update_buf, &offset, (uint8_t *)&value1, sizeof(value1));
-  append_buf(data_update_buf, &offset, (uint8_t *)&value2, sizeof(value2));
-  append_buf(data_update_buf, &offset, (uint8_t *)&value3, sizeof(value3));
-  append_buf(data_update_buf, &offset, (uint8_t *)&value4, sizeof(value4));
-  return offset;
+///   return  -   size of bytes written on success; otherwise return 0
+
+uint32_t device_write(uint8_t param, uint8_t* data, size_t len) {
+  switch (param) {
+    case VALUE_0:
+      value0 = (uint8_t) read_num_bytes(data, len);
+      return sizeof(value0);
+      break;
+    case VALUE_1:
+      value1 = (uint8_t) read_num_bytes(data, len);
+      return sizeof(value1);
+      break;
+    case VALUE_2:
+      value2 = (uint16_t) read_num_bytes(data, len);
+      return sizeof(value2);
+      break;
+    case VALUE_3:
+      value3 = (uint32_t) read_num_bytes(data, len);
+      return sizeof(value3);
+      break;
+    case VALUE_4:
+      value4 = (uint64_t) read_num_bytes(data, len);
+      return sizeof(value4);
+      break;
+    case VALUE_5:
+      value5 = (uint64_t) read_num_bytes(data, len);
+      return sizeof(value5);
+      break;
+    default:
+      return 0;
+  }
+  return 0;
 }
 
+
+// You must implement this function.
+// It is called when the device receives a Device Data Update packet.
+// Modifies data_update_buf to contain the parameter value.
+//    param           -   Parameter index
+//    data_update_buf -   buffer to return data in
+//    buf_len         -   Maximum length of the buffer? TODO: Clarify
+// TODO: Endianess? Currently both are little endian
+//    return          -   sizeof(param) on success; 0 otherwise
+
+uint8_t device_data_update(int param, uint8_t* data_update_buf, size_t buf_len) {
+  switch (param) {
+    case VALUE_0:
+      write_num_bytes(value0, data_update_buf, buf_len);
+      return sizeof(value0);
+      break;
+    case VALUE_1:
+      write_num_bytes(value0, data_update_buf, buf_len);
+      return sizeof(value1);
+      break;
+    case VALUE_2:
+      write_num_bytes(value0, data_update_buf, buf_len);
+      return sizeof(value2);
+      break;
+    case VALUE_3:
+      write_num_bytes(value0, data_update_buf, buf_len);
+      return sizeof(value3);
+      break;
+    case VALUE_4:
+      write_num_bytes(value0, data_update_buf, buf_len);
+      return sizeof(value4);
+      break;
+    case VALUE_5:
+      write_num_bytes(value0, data_update_buf, buf_len);
+      return sizeof(value5);
+      break;
+    default:
+      return 0;
+  }
+  return 0;
+}
+
+
+// Assumes little endian
+
+uint64_t read_num_bytes(uint8_t* data, size_t num) {
+  uint64_t value = 0;
+  for (size_t i = 0; i < num; i++) {
+    value += data[i] << (8 * i);
+  }
+  return value;
+}
+
+
+// Assumes little endian
+
+void write_num_bytes(uint64_t data, uint8_t* data_buf, size_t num) {
+  for (size_t i = 0; i < num; i++) {
+    data_buf[i] = (uint8_t) (data >> (8 * i));
+  }
+}
--- hibike/devices/ExampleDevice/example_device.h
+++ hibike/devices/ExampleDevice/example_device.h
@@ -13,6 +13,14 @@ hibike_uid_t UID = {
 
 #define NUM_PARAMS 6 //change to however many params are needed
 
+typedef enum {
+  VALUE_0 = 0,
+  VALUE_1 = 1,
+  VALUE_2 = 2,
+  VALUE_3 = 3,
+  VALUE_4 = 4,
+  VALUE_5 = 5
+} param;
 
 // function prototypes
 void setup();
@@ -20,5 +28,10 @@ void loop();
 
 uint8_t device_data_update(int param, uint8_t* data_update_buf, size_t buf_len);
 uint32_t device_write(uint8_t param, uint8_t* data, size_t len);
+
+
+// Utility functions
+uint64_t read_num_bytes(uint8_t* data, size_t num);
+void write_num_bytes(uint64_t data, uint8_t* data_buf, size_t num);
 
 #endif /* EX_DEVICE_H */
