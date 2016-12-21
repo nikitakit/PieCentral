9bf64d325438e832bd03f6faa824535d34f809c5
Yizhe Liu <yizow@berkeley.edu>
2016-12-20 20:26:28 -0800

    [HIBIKE] Update ServoControl device
    In function device_write, changed 'params' to 'param'. Hopefully this
    was the intended function.
    Fixed a few sytanx bugs.
    Deleted commented out code.
    Made whitespace consistent.

--- hibike/devices/ServoControl/servo_control.cpp
+++ hibike/devices/ServoControl/servo_control.cpp
@@ -2,13 +2,6 @@
 
 Servo servos[NUM_PINS];
 
-/*uint64_t prevTime, currTime, heartbeat;
-uint8_t param, servo;
-uint32_t value;
-uint16_t subDelay;
-bool led_enabled;
-*/
-
 uint8_t pins[NUM_PINS] = {SERVO_0, SERVO_1};
 
 volatile uint8_t toggle0 = 1; // 0 means servo off, 1 means on
@@ -21,7 +14,6 @@ void setup() {
   for (int i = 0; i < NUM_PINS; i++) {
     servos[i].attach(pins[i]);
   }
-
 }
 
 
@@ -29,15 +21,23 @@ void loop() {
   hibike_loop();
 }
 
-// you must implement this function. It is called when the device receives a DeviceUpdate packet.
-// the return value is the value field of the DeviceRespond packet hibike will respond with
+
+// You must implement this function.
+// It is called when the device receives a Device Write packet.
+// Updates param to new value passed in data.
+//    param   -   Parameter index
+//    data    -   value to write, in bytes TODO: What endian?
+//    len     -   number of bytes in data
+//
+//   return  -   size of bytes written on success; otherwise return 0
+
 uint32_t device_write(uint8_t param, uint8_t* value, size_t len) {
   if(param< NUM_PINS && len< sizeof(servos[param]) || len< sizeof(toggle0)){
     return 0;
   }
 
   if (param < NUM_PINS) {
-    servos[param].write((value[0]);
+    servos[param].write(value[0]);
     return sizeof(value[0]);
   }
 
@@ -53,22 +53,13 @@ uint32_t device_write(uint8_t param, uint8_t* value, size_t len) {
       return sizeof(toggle_servo(1, toggle1));
       break;
 
-    // case TOGGLE2:
-    //   toggle2 = value[0];
-    //   return sizeof(toggle_servo(2, toggle2));
-    //   break;
-
-    // case TOGGLE3:
-    //   toggle3 = value[0];
-    //   return sizeof(toggle_servo(3, toggle3));
-    //   break;
-
     default:
       return 0;
   }
 }
 
-uint32_t toggle_servo(int servo_num, uint32_t toggle) {
+
+uint8_t toggle_servo(int servo_num, uint8_t toggle) {
   if ((toggle == 1) && (!servos[servo_num].attached())) {
     servos[servo_num].attach(pins[servo_num]);
   } else if ((toggle == 0) && (servos[servo_num].attached())) {
@@ -80,38 +71,28 @@ uint32_t toggle_servo(int servo_num, uint32_t toggle) {
   return toggle;
 }
 
-// you must implement this function. It is called when the deviCe receives a DeviceStatus packet.
-// the return value is the value field of the DeviceRespond packet hibike will respond with
-uint32_t device_status(uint8_t param) {
-  if (param < NUM_PINS) {
-    return servos[param].read();
-  }
-  return ~((uint32_t) 0);
-}
 
-
-// you must implement this function. It is called with a buffer and a maximum buffer size.
-// The buffer should be filled with appropriate data for a DataUpdate packer, and the number of bytes
-// added to the buffer should be returned. 
+// You must implement this function.
+// It is called when the device receives a Device Data Update packet.
+// Modifies data_update_buf to contain the parameter value.
+//    param           -   Parameter index
+//    data_update_buf -   buffer to return data in
+//    buf_len         -   Maximum length of the buffer? TODO: Clarify
 //
-// You can use the helper function append_buf.
-// append_buf copies the specified amount data into the dst buffer and increments the offset
-uint8_t data_update(uint8_t* data_update_buf, size_t buf_len) {
-  uint8_t offset = 0;
-  return offset;
-}
+//    return          -   sizeof(param) on success; 0 otherwise
 
 uint8_t device_data_update(uint8_t param, uint8_t* data_update_buf, size_t buf_len) {
-  if(MAX_PAYLOAD_SIZE - buf_len < sizeof(uint8_t)|| param>NUM_PINS*2 || param < 0){
+  if (MAX_PAYLOAD_SIZE - buf_len < sizeof(uint8_t) ||
+      param > NUM_PINS * 2) {
     return 0;
   }
-  if(param<2){
+  if (param<2) {
     data_update_buf[0] = ((uint8_t)servos[param].read()); //32 bit return, must cast to 8 bit buffer
   }
-  else if (params == NUM_PINS){
+  else if (param == NUM_PINS) {
     data_update_buf[0] = toggle0;
   }
-  else if (params == NUM_PINS+1){
+  else if (param == NUM_PINS + 1) {
     data_update_buf[0] = toggle1;
   }
   
