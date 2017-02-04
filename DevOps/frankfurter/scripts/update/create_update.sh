022e9cd905de5aaefb8b3ac242c05d1579ea5f80
Arjun Sridhar <arjunsridhar1@gmail.com>
2017-02-04 14:35:30 -0800

    added protobuf compiler to update script

--- DevOps/frankfurter/scripts/update/create_update.sh
+++ DevOps/frankfurter/scripts/update/create_update.sh
@@ -8,6 +8,8 @@
 FRANKFURTER_DIR=$(git rev-parse --show-toplevel)/DevOps/frankfurter
 BUILD_DIR=$FRANKFURTER_DIR/build
 TMP_DIR=$BUILD_DIR/tmp
+PROTO_DIR=$(git rev-parse --show-toplevel)/ansible-protos
+RUNTIME_PROTO=$(git rev-parse --show-toplevel)/runtime/testy
 
 if [ -d "$BUILD_DIR" ]; then
     echo "Cleaning old builds ... "
@@ -19,7 +21,15 @@ fi
 mkdir -p $TMP_DIR
 echo "Done."
 
+
 git pull origin master
+<<<<<<< HEAD
+
+protoc PROTO_DIR/runtime.proto --python_out=RUNTIME_PROTO
+
+
+=======
+>>>>>>> d3830b575389ba7d7b040c492c105e04b216b61a
 # Copy hibike and runtime
 cp -R $(git rev-parse --show-toplevel)/hibike $TMP_DIR/hibike
 cp -R $(git rev-parse --show-toplevel)/runtime $TMP_DIR/runtime
